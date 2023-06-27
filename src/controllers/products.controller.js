87b1539608548db4701c37d7ed222e0e9630632d
Luis Urbina <urbinaismael56@gmail.com>
2023-06-27 12:30:30 -0600

    CRUD terminado

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -30,7 +30,7 @@ export const createProduct = async (req,res) =>{
         .input("price",sql.Money,price)
         .input("quantity",sql.Int,quantity)
         .query("insert into products values (@namepr,@description,@price,@quantity)")
-        console.log("Ingresado...");
+        console.log("Done...");
         res.json("New product")
         pool.close();
     }catch(err){
@@ -45,11 +45,51 @@ export const delProduct = async(req,res)=>{
         await pool.request()
         .input("id",sql.Int,id)
         .query("delete from products where id = @id")
-        console.log("Producto eliminado...");
+        console.log("Done...");
         res.json("Deleted")
         pool.close();
     }catch(err){
-        res.send('wtf')
         console.log(err);
     }    
 }
+
+export const updateProduct = async (req,res) =>{
+    console.log("sdadadasdasd")
+    const {namepr,description, price,quantity} = req.body
+    const {id}  = req.params
+    if(namepr==null || description == null || price == null || quantity == null){
+        res.status(400).json({msg : "Bad Request. Please fill all fields..."})
+    }
+
+   
+    try{
+        const pool = await getConnection();
+        await pool.request()
+        .input("namepr",sql.VarChar,namepr)
+        .input("description",sql.Text,description)
+        .input("price",sql.Money,price)
+        .input("quantity",sql.Int,quantity)
+        .input("id",sql.Int,id)
+        .query("update products set namepr = @namepr,description = @description,price = @price,quantity = @quantity where id = @id")
+        console.log("Done...");
+        res.json("Product updated")
+        pool.close();
+    }catch(err){
+        console.log(err);
+    }
+};
+
+export const getProductById =  async (req,res)  => {
+    try{
+        const {id} = req.params
+        const pool = await getConnection();
+        const result = await pool.request()
+        .input("id",sql.Int,id)
+        .query("select * from products where id=@id")
+        console.log(result);
+        res.json(result.recordset);
+        pool.close();
+    }catch(err){
+        console.log(err);
+    }
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { createProduct, delProduct, getProducts } from '../controllers/products.controller'
+import { createProduct, delProduct, getProductById, getProducts, updateProduct } from '../controllers/products.controller'
 
 const router = Router()
 router.get('/products',getProducts)
 
+router.get('/products/:id',getProductById)
+
 router.post('/products',createProduct)
 
 router.delete('/products/:id',delProduct)
+
+router.put('/products/:id',updateProduct)
+
 
 export default router
